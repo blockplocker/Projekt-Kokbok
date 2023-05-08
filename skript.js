27233188adcfdba53ac8eef41c0d90e1a79549d4
blockplocker <noahallklint@gmail.com>
2023-05-08 14:43:59 +0200

    layout and 3 new java skript functions

---
+++ skript.js
@@ -0,0 +1,41 @@
+function scrollToElement(id) {
+    var element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
+
+"use strict";
+
+const array = [1, 2, 3, 4];
+const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+const collection = document.getElementsByClassName("recept");
+
+let i = 0;
+
+for (const element of collection) {
+    element.style.order = shuffledArray[i];
+    i++;
+}
+
+
+
+
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    var TopButton = document.getElementById("TopButton");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        TopButton.style.display = "block";
+    } else {
+        TopButton.style.display = "none";
+    }
+}
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
